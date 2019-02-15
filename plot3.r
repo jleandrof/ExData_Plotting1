1ac4ddad761bc261d9e1822795dc6bc4afcd84ca
Marco Antonio Souza <maps@ic.ufal.br>
2019-02-14 23:07:59 -0300

    plot 3

---
+++ plot3.r
@@ -0,0 +1,26 @@
+## Plot 3
+
+
+# Loading dataset into R.
+data = read.table("household_power_consumption.txt", header=TRUE, sep = ";", na.strings = "?")
+
+# Removing missing values
+data <- na.omit(data)
+
+# Converting date format
+data$Date = as.Date(data$Date, format="%d/%m/%Y")
+as.Da
+# Defining time period
+time_period = data[(data$Date >= as.Date("2007-02-01")) & (data$Date <= as.Date("2007-02-02")), ]
+
+#Ploting
+plot((as.POSIXct(paste(time_period$Date, time_period$Time), "%d/%m/%Y %H:%M:%S")), as.numeric(time_period$Sub_metering_1), type="l", ylab = "Global Active Power (kilowatts)", xlab = "")
+lines((as.POSIXct(paste(time_period$Date, time_period$Time), "%d/%m/%Y %H:%M:%S")), as.numeric(time_period$Sub_metering_2), type="l", ylab = "Global Active Power (kilowatts)", xlab = "", col="red")
+lines((as.POSIXct(paste(time_period$Date, time_period$Time), "%d/%m/%Y %H:%M:%S")), as.numeric(time_period$Sub_metering_3), type="l", ylab = "Global Active Power (kilowatts)", xlab = "", col="blue")
+
+legend(x = "topright", lwd=1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3") )
+
+# Save png
+dev.copy(png, file = "plot3.png")
+dev.off()
+
