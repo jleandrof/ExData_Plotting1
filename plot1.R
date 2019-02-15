ba130a9289adc3d18e58da3bca90b9f0d249c7b4
Leandro Silva <jlsf@ic.ufal.br>
2019-02-14 21:40:55 -0300

    Add first plot

---
+++ plot1.R
@@ -0,0 +1,19 @@
+## Plot 1
+
+# Downloading file and reading as a dataframe
+temp <- tempfile()
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+data <- read.csv(unz(temp, "household_power_consumption.txt"), sep=';', na.strings = "?")
+unlink(temp)
+
+# Removing missing values
+data <- na.omit(data)
+
+# Converting date format
+data$Date = as.Date(data$Date, format="%d/%m/%Y")
+
+# Defining time period
+time_period = data[(data$Date >= as.Date("2007-02-01")) & (data$Date <= as.Date("2007-02-02")), ]
+
+# Ploting histogram
+hist(time_period$Global_active_power, col="red", main = "Global Active Power", xlab = "Global Active Power (Kilowatts)")
